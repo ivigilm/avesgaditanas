461bc82db5e9501dd7453383e05c16ae99a9a9c0
Isabel <isabel.vigil.moran@gmail.com>
2019-06-05 21:12:47 +0100

    Se ordenan los resultados por nombre

--- lecturasp.php
+++ lecturasp.php
@@ -2,7 +2,7 @@
 $c = new mysqli("localhost", "root", "", "avesgaditanas");
 $c -> query("SET NAMES utf8");
 $salida = array();
-$resultado = $c -> query("SELECT * FROM ave");
+$resultado = $c -> query("SELECT * FROM ave ORDER BY ave.nombrecomun");
 $salida = $resultado -> fetch_all(MYSQLI_ASSOC);
 echo json_encode($salida);
 $c -> close();
--- lecturazonas.php
+++ lecturazonas.php
@@ -3,7 +3,8 @@ $c = new mysqli("localhost", "root", "", "avesgaditanas");
 $c -> query("SET NAMES utf8");
 $salida = array();
 $resultado = $c -> query("SELECT id, nombre 
-                        FROM comarca");
+                        FROM comarca
+                        ORDER BY comarca.nombre");
 $salida = $resultado -> fetch_all(MYSQLI_ASSOC);
 echo json_encode($salida);
 $c -> close();
